8c2398b8a92b2fca5ae881405af55522721b91f9
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-12 09:44:17 +0000

    added display weather function added

--- src/interface.js
+++ src/interface.js
@@ -28,24 +28,28 @@ $(document).ready(function () {
         $('#power-saving-mode').text('off');
       });
 
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a8a9d855f19ccd6a35cc2c6bf8328d89&units=metric', function (data) {
-        $('#current-temperature').text(data.main.temp);
-        $('#location').text('London');
-      });
+    displayWeather('London');
 
     $('#select-city').submit(function (event) {
       event.preventDefault();
       let city = $('#current-city').val();
-      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a8a9d855f19ccd6a35cc2c6bf8328d89&units=metric', function (data) {
-        $('#current-temperature').text(data.main.temp);
-        $('#location').text(city);
-      });
+      displayWeather(city);
     });
 
     function updateTemperature() {
       $('#temperature').text(thermostat.currentTemp());
       $('#power-usage').text(thermostat.energyUsage());
       $('#power-usage').attr('class', thermostat.energyUsage());
+    }
+
+    function displayWeather(city) {
+      let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      let token = '&appid=a8a9d855f19ccd6a35cc2c6bf8328d89';
+      let units = '&units=metric';
+      $.get(url + token + units, function (data) {
+        $('#current-temperature').text(data.main.temp);
+        $('#location').text(city);
+      });
     }
 
   });
